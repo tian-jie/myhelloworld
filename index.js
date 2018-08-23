cbb4a893fa7785f812281e7073872ac14c9ac033
TianJie <tianjie@ICs-MacBook-Air.local>
2018-08-23 21:24:41 +0800

    add gitignore

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/a', (req, res) => res.send('Hello World! Are you A?'))
+function AAction(req, res){
+    res.send('Hello World!')
+}
+function HomeAction(req, res){
+    res.send('Hello World!')
+}
+
+app.get('/', HomeAction)
+app.get('/a', AAction)
 app.get('/b', (req, res) => res.send('Hello World! Are you B?'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
