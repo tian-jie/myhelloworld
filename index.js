e30130e4cf11cf6001a58a1a64c7893ba5a3c8b8
TianJie <tianjie@ICs-MacBook-Air.local>
2018-08-29 08:24:10 +0800

    trying

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
 const express = require('express')
 const app = express()
 
-function AAction(req, res){
-    res.send('Hello World!')
-}
-function HomeAction(req, res){
-    res.send('Hello World!')
-}
+var WechatAPI = require('wechat-api');
+var token ='Innocellence'
+var appid = 'wxd7bf18ecdc988273'
+var appsecret = 'bd585de3f32746b7d3fc6044235248fd'
+var encodingAESKey = 'EluZag9AnbjTapoGu44lcIHmVfoPl2DCqzpNcwbTfHG'
 
-app.get('/', HomeAction)
-app.get('/a', AAction)
-app.get('/b', (req, res) => res.send('Hello World! Are you B?'))
+var api = new WechatAPI(appid, appsecret);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var wechat = require('wechat');
+var config = {
+  token: token,
+  appid: appid,
+  encodingAESKey: encodingAESKey,
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
 
+app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
+
+//引入http模块
+var http = require("http");
+//设置主机名
+var hostName = '127.0.0.1';
+//设置端口
+var port = 808;
+//创建服务
+var server = http.createServer(function(req,res){
+    res.setHeader('Content-Type','text/plain');
+    res.end("hello nodejs");
+
+});
+server.listen(port,hostName,function(){
+    console.log(`服务器运行在http://${hostName}:${port}`);
+});
